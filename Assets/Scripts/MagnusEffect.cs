c5c85e15522e7cbfc73bd180e82c712a810cbd62
wiyadathi <wiyada.t@bu.ac.th>
2025-03-15 14:21:33 +0700

    magnus effect comfplete with sample scene

--- Assets/Scripts/MagnusEffect.cs
+++ Assets/Scripts/MagnusEffect.cs
@@ -2,15 +2,37 @@ using UnityEngine;
 
 public class MagnusEffect : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    Rigidbody rb;
+    [SerializeField] Vector3 velocity, spin;
+    
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (Input.GetKey(KeyCode.Space))
+        {
+            Kick();
+        }
+
+        ApplyMagnusEffect();
+    }
+    void Kick() 
+    {
+        // ให้บอลเคลื่อนที่ไปข้างหน้าพร้อมกับการหมุน
+        rb.linearVelocity = velocity;
+        rb.angularVelocity = spin;
+    }
+
+
+    void ApplyMagnusEffect()
+    {
+        //คำนวณแรงแมกนัสจาก Cross Product ของความเร็วและการหมุน
+        Vector3 magnusForce  = Vector3.Cross(rb.linearVelocity, rb.angularVelocity);
+            
+         //apply magnus force to the ball-RB 
+         rb.AddForce(magnusForce, ForceMode.Force); //ทำให้ลูกบอลโค้ง
     }
 }
